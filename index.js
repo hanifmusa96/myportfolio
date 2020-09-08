2406492527ac01b4c916f1bedf19b97166458aa8
hanifmusa96 <hanifmusa96@users.noreply.github.com>
2020-09-08 12:09:29 +0800

    correction on css

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('assets'))
+
 app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
   res.render('home')
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
